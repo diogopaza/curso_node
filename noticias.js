58776b614ce1227e5b43436d91f9add440650b7f
diogo paza <diogopazacvel@gmail.com>
2020-02-20 19:11:57 -0300

    adicionando rotas

--- noticias.js
+++ noticias.js
@@ -1,7 +1,21 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-    res.end("<h1>Servidor Rodando</h1>")
+
+    var categoria = req.url
+    console.log("categoria: " + categoria)
+    if(categoria == '/tecnologia'){
+        res.end("<h1>Noticias de tecnologia</h1>")
+    }
+    if(categoria == '/moda'){
+        res.end("<h1>Noticias de moda</h1>")
+    }
+    if(categoria == '/beleza'){
+        res.end("<h1>Noticias de beleza</h1>")
+    }else{
+        res.end("<h1>Servidor Rodando</h1>")
+    }
+   
 });
 
 server.listen(3000);
