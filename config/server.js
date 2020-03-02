e3e94e8b53fed04cdc377420977fc5da40898de5
diogo paza <diogopazacvel@gmail.com>
2020-03-01 23:00:38 -0300

    add validacoes

--- config/server.js
+++ config/server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var {check, validationResult}  = require('express-validator');
 var application = express();
 
 application.set('view engine', 'ejs');
 application.set('views', './app/views');
 
 application.use(bodyParser.urlencoded({extended:true}));
+application.use(check());
 
 consign()
     .include('app/routes')
